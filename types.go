fa4baf9fb8f258a4312013838f7f216458d2850c
Shiraaz Moollatjie <mllshi011@gmail.com>
2019-10-23 23:15:14 +0200

    articles: Fixing comments

--- types.go
+++ types.go
@@ -7,12 +7,14 @@ type emptyTime struct {
 	*time.Time
 }
 
+// UnmarshalJSON is a copy of the time.Time UnmarshalJSON function
+// with an extra check for an empty string
 func (m *emptyTime) UnmarshalJSON(data []byte) error {
-	// Ignore null, like in the main JSON package.
+
 	if string(data) == "null" || string(data) == `""` {
 		return nil
 	}
-	// Fractional seconds are handled implicitly by Parse.
+
 	tt, err := time.Parse(`"`+time.RFC3339+`"`, string(data))
 	*m = emptyTime{&tt}
 	return err
